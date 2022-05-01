99c6702e62af0504fbe3b1b9e39a497a571d3992
彭芳洁 <1639579565@qq.com>
2022-05-01 16:22:23 +0800

    存在重复元素

---
+++ golang/217.存在重复元素.go
@@ -0,0 +1,13 @@
+package golang
+
+// hash
+func containsDuplicate(nums []int) bool {
+	m := map[int]int{}
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			return true
+		}
+		m[v] = 1
+	}
+	return false
+}
