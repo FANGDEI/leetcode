129b1c9e9b56d340000e371864f005216d5be6e7
彭芳洁 <1639579565@qq.com>
2022-05-06 12:57:46 +0800

    路径总和II

---
+++ golang/113.路径总和II.go
@@ -0,0 +1,41 @@
+package golang
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 全局变量
+var res = [][]int{}
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	// leetcode 全局变量只会初始化一次
+	res = [][]int{}
+	nowPath := []int{}
+	dfs(root, nowPath, targetSum, 0)
+	return res
+}
+
+// 在路径总和I的代码中加一个记录路径的变量即可
+func dfs(root *TreeNode, nowPath []int, targetSum, sum int) {
+	if root == nil {
+		return
+	}
+	sum += root.Val
+	nowPath = append(nowPath, root.Val)
+	defer func() {
+		// 回溯
+		nowPath = nowPath[:len(nowPath)-1]
+	}()
+	if root.Left == nil && root.Right == nil && sum == targetSum {
+		cp := make([]int, len(nowPath))
+		copy(cp, nowPath)
+		res = append(res, cp)
+		// golang slice 的注意点
+		// res = append(res, nowPath)
+		return
+	}
+	dfs(root.Left, nowPath, targetSum, sum)
+	dfs(root.Right, nowPath, targetSum, sum)
+}
