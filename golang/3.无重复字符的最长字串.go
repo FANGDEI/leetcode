94c162baf9b64d899197ab1496f79d440fef0a79
彭芳洁 <1639579565@qq.com>
2022-05-11 18:16:13 +0800

    无重复字符的最长字串

---
+++ golang/3.无重复字符的最长字串.go
@@ -0,0 +1,26 @@
+package golang
+
+// 双指针 + map
+// map用来记录当前序列每个字符出现的次数
+// 当map中的记录大于一时说明当前序列出现重复
+// 移动指针直到重复字符消失即可
+func lengthOfLongestSubstring(s string) int {
+	a := map[byte]int{}
+	res := 0
+	for i, j := 0, 0; i < len(s); i++ {
+		a[s[i]]++
+		for j <= i && a[s[i]] > 1 {
+			a[s[j]]--
+			j++
+		}
+		res = Max(res, i-j+1)
+	}
+	return res
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
