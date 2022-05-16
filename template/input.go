00274dd42414483d7a5ac466c6b6d1d94ff7d305
彭芳洁 <1639579565@qq.com>
2022-05-16 20:19:15 +0800

    feat: input.go

---
+++ template/input.go
@@ -0,0 +1,20 @@
+package template
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+var scanner *bufio.Scanner
+
+func new(reader io.Reader) {
+	scanner = bufio.NewScanner(reader)
+	bs := make([]byte, 20000*1024)
+	scanner.Buffer(bs, len(bs))
+}
+
+func readLine() []string {
+	scanner.Scan()
+	return strings.Split(scanner.Text(), " ")
+}
