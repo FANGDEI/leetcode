4b605e315674cf9cdbdbb161463d2b36d679a86e
彭芳洁 <1639579565@qq.com>
2022-05-20 01:13:34 +0800

    feat: backpack

--- template/dp/backpack/多重背包.go
+++ template/dp/backpack/多重背包.go
@@ -2,7 +2,30 @@ package backpack
 
 import "fmt"
 
+// MultiplePack 朴素
 func MultiplePack() {
+	var (
+		n, m    int
+		v, w, s [1005]int
+		f       [1005]int
+	)
+
+	fmt.Scan(&n, &m)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&v[i], &w[i], &s[i])
+	}
+	for i := 1; i <= n; i++ {
+		for j := m; j >= v[i]; j-- {
+			for k := 0; k <= s[i] && k*v[i] <= j; k++ {
+				f[j] = max(f[j], f[j-k*v[i]]+k*w[i])
+			}
+		}
+	}
+	fmt.Println(f[m])
+
+}
+
+func multiplePack() {
 	var (
 		n, m    int
 		v, w, s [1005]int
--- template/dp/backpack/完全背包.go
+++ template/dp/backpack/完全背包.go
@@ -2,6 +2,30 @@ package backpack
 
 import "fmt"
 
+// CompletePack 朴素
+func CompletePack() {
+	var (
+		n, m int
+		v, w [1005]int
+		f    [1005][1005]int
+	)
+
+	fmt.Scan(&n, &m)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&v[i], &w[i])
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			f[i][j] = f[i-1][j]
+			if j >= v[i] {
+				f[i][j] = max(f[i][j], f[i][j-v[i]]+w[i])
+			}
+		}
+	}
+	fmt.Println(f[n][m])
+
+}
+
 // 空间优化后
 func completePack() {
 	var (
