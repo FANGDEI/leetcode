e47649293594b3bea55e6063326c146b48a23404
彭芳洁 <1639579565@qq.com>
2022-05-24 23:18:34 +0800

    feat: leetcode

---
+++ golang/26..删除有序数组中的重复项.go
@@ -0,0 +1,14 @@
+package golang
+
+func removeDuplicates(nums []int) int {
+	i, j := 1, 0
+	for i < len(nums) {
+		if nums[i] != nums[j] {
+			j++
+			nums[j] = nums[i]
+		}
+		i++
+	}
+	j++
+	return j
+}
