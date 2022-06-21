e30a879021c613ff9edf0271c1858f33eaa4807a
Devildyw <1842501760@qq.com>
2022-06-21 13:12:22 +0800

    :shipit: 860.柠檬水找零 867.转置矩阵 242.有效的字母异位词 Date:2022/6/21

---
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/arrays/L867转置矩阵.java
@@ -0,0 +1,32 @@
+package com.dyw.leetcode.simple.arrays;
+
+/**
+ * @author Devil
+ * @since 2022-06-21-12:44
+ */
+@SuppressWarnings("all")
+public class L867转置矩阵 {
+    public static void main(String[] args) {
+        int[][] transpose = new L867转置矩阵().transpose(new int[][]{{1,2,3},{4,5,6}});
+        for (int[] ints : transpose) {
+            for (int i = 0; i < ints.length; i++) {
+                System.out.print(ints[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] res = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+
+        return res;
+    }
+}
