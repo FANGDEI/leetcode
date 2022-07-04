164a29e4d098cc0ced3252c221f874fa3089aadf
Devildyw <1842501760@qq.com>
2022-07-04 14:02:44 +0800

    941.有效的山脉数组 696.计数二进制子串 709.转换成小写字母 771.宝石与石头 796.旋转字符串 Date:2022/7/4

---
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/string/L796旋转字符串.java
@@ -0,0 +1,38 @@
+package com.dyw.leetcode.simple.string;
+
+/**
+ * @author Devil
+ * @since 2022-07-04-13:25
+ *
+ * 给定两个字符串, s和goal。如果在若干次旋转操作之后，s能变成goal，那么返回true。
+ *
+ * s的 旋转操作 就是将s 最左边的字符移动到最右边。
+ *
+ * 例如, 若s = 'abcde'，在旋转一次之后结果就是'bcdea'
+ *
+ *
+ */
+@SuppressWarnings("all")
+public class L796旋转字符串 {
+    public static void main(String[] args) {
+        System.out.println(new L796旋转字符串().rotateString("abcde", "cdeab"));
+    }
+
+    public boolean rotateString(String s, String goal) {
+        int n = goal.indexOf(s.charAt(0))+s.length();
+        char[] chars = s.toCharArray();
+        int m = chars.length;
+        char temp = 0;
+        while (n-->0){
+            temp = chars[0];
+            for (int i = 1; i < m; i++) {
+                chars[i-1] = chars[i];
+            }
+            chars[m-1] = temp;
+            if (new String(chars).equals(goal)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
