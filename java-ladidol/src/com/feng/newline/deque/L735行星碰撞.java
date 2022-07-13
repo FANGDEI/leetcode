c8685cb03294b30ab7ce5681ac634b95143993c7
ladidol <599426945@qq.com>
2022-07-13 12:55:40 +0800

    :alembic: 每日一题

---
+++ java-ladidol/src/com/feng/newline/deque/L735行星碰撞.java
@@ -0,0 +1,102 @@
+package com.feng.newline.deque;
+
+import java.util.*;
+
+/**
+ * @projectName: leetcode
+ * @package: com.feng.newline.deque
+ * @className: L735行星碰撞
+ * @author: Ladidol
+ * @description: 若当前行星是大于0的，直接入栈。
+ * 若当前行星是小于0的，我们考虑在什么情况下会发生碰撞，并且判断当前行星是否存活
+ * <p>
+ * 若栈为空，直接入栈
+ * 若栈顶元素小于0，表示栈内元素均向左，直接入栈
+ * 若栈顶元素大于0，我们需要和当前行星做判断：
+ * 绝对值相等，均爆炸
+ * 栈顶元素大于当前行星绝对值，当前行星爆炸
+ * 栈顶元素小于当前行星绝对值，栈顶元素爆炸，然后接着判断下一个栈顶元素。
+ * @date: 2022/7/13 9:36
+ * @version: 1.0
+ */
+public class L735行星碰撞 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, -2, -3};
+        System.out.println("new Solution().asteroidCollision() = " + Arrays.toString(new Solution().asteroidCollision(nums)));
+    }
+
+    //deque = [5, 4, 3, 2, 1] 队列原本的样子.
+    //new Solution().asteroidCollision() = [1, 2, 3, 4, 5]
+
+
+    //模拟栈, 栈顶就是队列头部,对列是一点一点往后面退.
+    static class Solution {
+        public int[] asteroidCollision(int[] asteroids) {
+
+            Deque<Integer> stack = new ArrayDeque<>();//2022/7/13 LinkedList和这个ArrayDeque的区别是.似乎没有区别
+
+            for (int asteroid : asteroids) {
+                if (asteroid > 0) {
+                    stack.addFirst(asteroid);//2022/7/13 push 和addLast 和addFirst有什么区别.push在队列首部
+//                    deque.push(asteroid);
+                } else {
+                    Boolean alive = true;
+                    while (alive && !stack.isEmpty() && stack.peek() > 0) {
+                        alive = stack.peek() < Math.abs(asteroid);
+                        if (stack.peek() <= Math.abs(asteroid)) {
+                            stack.pop();
+                        }
+                    }
+                    if (alive) {
+                        stack.push(asteroid);
+                    }
+                }
+            }
+            int n = stack.size();
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++) {//注意这里每次poll一下,size都会变小的,size可以用n来代替
+                ans[i] = stack.pollLast();
+//                ans[i++] = stack.pop();//pop和poll是一样的都是从前面开始移除.
+//                ans[i++] = stack.poll();
+            }
+            return ans;
+        }
+    }
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int asteroid : asteroids) {
+            //大于0 直接入栈
+            if (asteroid > 0) {
+                stack.push(asteroid);
+            } else {
+                //是否存活
+                boolean alive = true;
+                //当前元素存活 并且栈不是空 并且栈顶元素大于0
+                while (alive && !stack.isEmpty() && stack.peek() > 0) {
+                    //只有栈顶元素小于当前元素的绝对值，才会存活
+                    alive = stack.peek() < Math.abs(asteroid);
+                    //栈顶元素爆炸（出栈）
+                    if (stack.peek() <= Math.abs(asteroid)) {
+                        stack.pop();
+                    }
+                }
+                if (alive) {
+                    stack.push(asteroid);
+                }
+            }
+        }
+        //结果输出
+        int[] ans = new int[stack.size()];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            ans[i++] = stack.pollLast();
+        }
+        return ans;
+    }
+
+
+}
+
+
---
+++ java-ladidol/src/com/feng/newline/deque/queue.md
@@ -0,0 +1,3 @@
+    //模拟栈, 栈顶就是队列头部,对列是一点一点往后面退.
+
+[Java双向队列Deque实现栈与队列_Ysming88的博客-CSDN博客_java双向队列](https://blog.csdn.net/m0_45861545/article/details/120720557?app_version=5.6.0&code=app_1562916241&csdn_share_tail={"type"%3A"blog"%2C"rType"%3A"article"%2C"rId"%3A"120720557"%2C"source"%3A"qq_51705526"}&ctrtid=Q6Pt8&uLinkId=usr1mkqgl919blen&utm_source=app)
