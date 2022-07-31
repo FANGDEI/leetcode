f7e2c29849f9f1ee7cf719d5c61063325336f01a
Devildyw <1842501760@qq.com>
2022-07-31 12:44:02 +0800

    字符串 Date:2022/7/30

--- java-devildyw/src/main/java/com/dyw/leetcode/simple/string/L290单词规律.java
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/string/L290单词规律.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 @SuppressWarnings("all")
 public class L290单词规律 {
     public static void main(String[] args) {
-        System.out.println(new L290单词规律().wordPattern("abba", "dog dog dog dog"));
+        System.out.println(new L290单词规律().wordPattern01("abba", "dog dog dog dog"));
     }
 
     public boolean wordPattern(String pattern, String s) {
@@ -28,6 +28,26 @@ public class L290单词规律 {
             }
             patternMap.put(c, str[i]);
             sMap.put(str[i], c);
+        }
+        return true;
+    }
+
+    public boolean wordPattern01(String pattern, String s) {
+        String[] split = s.split("[ ]+");
+        if (split.length!=pattern.length()){
+            return false;
+        }
+        //使用两个map分别存储字符与字符串的对应关系 防止出现 abba dog dog dog dog 为true的情况
+        //该情况就是没有字符对应字符串的情况 我们需要保证字符与字符串一一对应 而map只是一个key对应一个值 所以我们用两个map就可以相互对应 达到一一对应
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> sMap = new HashMap<>();
+        for (int i = 0; i < split.length; i++) {
+            char c = pattern.charAt(i);
+            if (map.containsKey(c)&&!map.get(c).equals(split[i])||(sMap.containsKey(split[i])&&sMap.get(split[i])!=(c))){
+                return false;
+            }
+            map.put(c,split[i]);
+            sMap.put(split[i],c);
         }
         return true;
     }
