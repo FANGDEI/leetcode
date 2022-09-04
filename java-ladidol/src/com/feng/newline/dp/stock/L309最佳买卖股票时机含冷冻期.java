502df1a1121fc284bea89d12e2e5f06120aeda64
ladidol <599426945@qq.com>
2022-09-05 00:48:35 +0800

    :alembic: 每日一题 数据结构——字典树

--- java-ladidol/src/com/feng/newline/dp/stock/L309最佳买卖股票时机含冷冻期.java
+++ java-ladidol/src/com/feng/newline/dp/stock/L309最佳买卖股票时机含冷冻期.java
@@ -23,6 +23,8 @@ package com.feng.newline.dp.stock;
  */
 public class L309最佳买卖股票时机含冷冻期 {
 
+
+    // 二维dp
     class Solution {
         // 共有三个状态：记住三个状态的每一种状态下对第二天的影响都是等价的，所以才会从中选择最大的。
         //1. 第i天不持有股票           【有情况有两种：a.第i-1天也不持有股票；b.第i-1天是卖出股票（今天是冷冻期）】
@@ -43,33 +45,6 @@ public class L309最佳买卖股票时机含冷冻期 {
             }
             //最后最大利润为最后一天，不持有股票或者进入冷冻期的情况
             return Math.max(dp[dp.length - 1][0], dp[dp.length - 1][2]);
-        }
-    }
-
-    class Solution {
-        public int maxProfit(int[] prices) {
-            if (prices == null || prices.length == 0) {
-                return 0;
-            }
-            //由于可以无限次交易，所以只定义两个维度，第一个维度是天数，第二个维度表示是否持有股票，0表示不持有，1表示持有，2表示过渡期
-            int[][] dp = new int[prices.length][3];
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
-            dp[0][2] = 0;
-            for (int i = 1; i < prices.length; i++) {
-                //第i天不持有股票的情况有两种
-                //a.第i-1天也不持有股票
-                //b.第i-1天是过渡期
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2]);
-                //第i天持有股票有两种情况
-                //a.第i-1天也持有股票，第i天不操作，
-                //b.第i-1天不持有股票，在第i天买入
-                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
-                //第i天是冷冻期只有一种情况，第i-1天持有股票且卖出
-                dp[i][2] = dp[i - 1][1] + prices[i];
-            }
-            //最后最大利润为最后一天，不持有股票或者进入冷冻期的情况
-            return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][2]);
         }
     }
 }
