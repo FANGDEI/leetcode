0bbc1e029d47cd834ce07d9e28c69026cbbbc4fc
Devildyw <1842501760@qq.com>
2022-09-06 12:06:26 +0800

    哈希表 Date:2022/9/6

---
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/map/L242有效的字母异位词.java
@@ -0,0 +1,44 @@
+package com.dyw.leetcode.simple.map;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * <p>
+ * 注意：若s 和 t中每个字符出现的次数都相同，则称s 和 t互为字母异位词。
+ *
+ * @author Devil
+ * @since 2022-09-06-10:47
+ */
+@SuppressWarnings("all")
+public class L242有效的字母异位词 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 使用数组来记录字母的出现次数 如果出现次数相同则代表是异位词
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[s.charAt(i) - 'a']--;
+        }
+
+        //判断是否有不为0出现次数的字母 返回false
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/map/L383赎金信.java
@@ -0,0 +1,50 @@
+package com.dyw.leetcode.simple.map;
+
+/**
+ * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+ * <p>
+ * 如果可以，返回 true ；否则返回 false 。
+ * <p>
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ *
+ * @author Devil
+ * @since 2022-09-06-10:53
+ */
+@SuppressWarnings("all")
+public class L383赎金信 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 数组也算是哈希表的一种哦
+     *
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            count[ransomNote.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < magazine.length(); i++) {
+            int index = magazine.charAt(i) - 'a';
+            if (count[index] > 0) {
+                count[index]--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
