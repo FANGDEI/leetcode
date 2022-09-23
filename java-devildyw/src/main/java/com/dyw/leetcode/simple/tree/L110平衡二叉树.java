b8169ed7e942e1b83ce2e143f2e026d424553a8b
Devildyw <1842501760@qq.com>
2022-09-23 11:47:54 +0800

    树 Date:2022/9/23

---
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/tree/L110平衡二叉树.java
@@ -0,0 +1,43 @@
+package com.dyw.leetcode.simple.tree;
+
+/**
+ * @author Devil
+ * @since 2022-09-23-11:35
+ */
+@SuppressWarnings("all")
+public class L110平衡二叉树 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 递归
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = getHeight(root.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        //左右子树高度差大于1，return -1 表示已经不是平衡树了
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        //人工高度差小于等于1，则返回最大的那个高度加1(加上这层的高度)
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
