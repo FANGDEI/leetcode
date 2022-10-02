20b4b1d9980805a9fbbd72c173f7523ff0af6cff
Devildyw <1842501760@qq.com>
2022-10-02 13:02:32 +0800

    树/每日一题 Date:2022/10/2

--- java-devildyw/src/main/java/com/dyw/leetcode/simple/tree/L700二叉搜索树中的搜索.java
+++ java-devildyw/src/main/java/com/dyw/leetcode/simple/tree/L700二叉搜索树中的搜索.java
@@ -33,4 +33,14 @@ public class L700二叉搜索树中的搜索 {
         }
         return searchBST(val < root.val ? root.left : root.right, val);
     }
+
+    public TreeNode searchBST01(TreeNode root, int val) {
+        if (root==null){
+            return null;
+        }
+        if (root.val==val){
+            return root;
+        }
+        return root.val<val?searchBST01(root.right,val):searchBST01(root.left,val);
+    }
 }
