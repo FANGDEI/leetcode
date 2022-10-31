4af7debd060804614b38a3e94a0293c6a55a5948
FANG <1639579565@qq.com>
2022-10-31 19:48:07 +0800

    简单题, 没破到哥们

--- golang/map/383.赎金信.go
+++ golang/map/383.赎金信.go
@@ -1,14 +1,14 @@
 package map_set
 
 func canConstruct(ransomNote string, magazine string) bool {
-	m := map[byte]int{}
+	m := map[uint8]int{}
 	for i := 0; i < len(magazine); i++ {
-		m[byte(magazine[i])]++
+		m[magazine[i]]++
 	}
 
 	for i := 0; i < len(ransomNote); i++ {
-		m[byte(ransomNote[i])]--
-		if cnt := m[byte(ransomNote[i])]; cnt < 0 {
+		m[magazine[i]]--
+		if cnt := m[magazine[i]]; cnt < 0 {
 			return false
 		}
 	}
