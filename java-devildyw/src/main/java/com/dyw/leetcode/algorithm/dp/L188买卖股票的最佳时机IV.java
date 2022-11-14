d8a1075703580824f48fb7cd74abfb84935c7a3a
Devildyw <1842501760@qq.com>
2022-11-14 13:30:19 +0800

    动态规划 Date:2022/11/14

--- java-devildyw/src/main/java/com/dyw/leetcode/algorithm/dp/L188买卖股票的最佳时机IV.java
+++ java-devildyw/src/main/java/com/dyw/leetcode/algorithm/dp/L188买卖股票的最佳时机IV.java
@@ -25,24 +25,26 @@ public class L188买卖股票的最佳时机IV {
      * @return
      */
     public int maxProfit(int k, int[] prices) {
-        if (prices.length==0) return 0;
-
+        if (prices.length==0){
+            return 0;
+        }
         int len = prices.length;
         // 股票状态: 奇数表示第 k 次交易持有/买入, 偶数表示第 k 次交易不持有/卖出, 0 表示没有操作
-        int[][] dp= new int[len][k*2+1];
+        int[][] dp= new int[len][2*k+1];
 
         //dp 数组初始化
-        for (int i = 1; i < k * 2; i+=2) {
-            dp[0][i] = -prices[0];
+        for (int j = 1; j < 2 * k; j+=2) {
+            dp[0][j] = -prices[0];
         }
 
         for (int i = 1; i < len; i++) {
-            for (int j = 0; j < k * 2 - 1; j+=2) {
+            for (int j = 0; j < 2 * k - 1; j+=2) {
                 dp[i][j+1] = Math.max(dp[i-1][j+1],dp[i-1][j]-prices[i]);
                 dp[i][j+2] = Math.max(dp[i-1][j+2],dp[i-1][j+1]+prices[i]);
             }
         }
 
-        return dp[len-1][k*2];
+        return dp[len-1][2*k];
+
     }
 }
