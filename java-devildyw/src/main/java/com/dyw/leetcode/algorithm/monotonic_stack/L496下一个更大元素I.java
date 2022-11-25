5cfb4de293debe86f30a975708e0402e09ea828f
Devildyw <1842501760@qq.com>
2022-11-25 12:51:08 +0800

    单调栈 每日一题 Date:2022/11/25

--- java-devildyw/src/main/java/com/dyw/leetcode/algorithm/monotonic_stack/L496下一个更大元素I.java
+++ java-devildyw/src/main/java/com/dyw/leetcode/algorithm/monotonic_stack/L496下一个更大元素I.java
@@ -15,6 +15,14 @@ public class L496下一个更大元素I {
         }
     }
 
+    /**
+     * 单调栈：
+     * 将nums1的元素与下标关系存入map中 因为nums1的元素是nums2的子集 所以对nums2构建单调栈 当遇到出栈的情况就代表当前遍历元素是第一个大于栈顶元素的值
+     * 取出栈顶元素对应的nums2的元素 根据map中的num1与下标的关系 更新ans 结果集中的结果
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
