88f1baca664b5245c095f7a931a81af18c062fb0
JirafaYe <2582101458@qq.com>
2022-11-30 21:21:51 +0800

    452. 用最少数量的箭引爆气球

---
+++ java-jirafa/src/main/java/com/jirafa/leetcode/algorithm/greedy/L452.java
@@ -0,0 +1,24 @@
+package com.jirafa.leetcode.algorithm.greedy;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons
+ */
+public class L452 {
+    public int findMinArrowShots(int[][] points) {
+        // 根据气球直径的开始坐标从小到大排序
+        // 使用Integer内置比较方法，不会溢出
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int count = 1;  // points 不为空至少需要一支箭
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > points[i - 1][1]) {  // 气球i和气球i-1不挨着，注意这里不是>=
+                count++; // 需要一支箭
+            } else {  // 气球i和气球i-1挨着
+                points[i][1] = Math.min(points[i][1], points[i - 1][1]); // 更新重叠气球最小右边界
+            }
+        }
+        return count;
+    }
+}
