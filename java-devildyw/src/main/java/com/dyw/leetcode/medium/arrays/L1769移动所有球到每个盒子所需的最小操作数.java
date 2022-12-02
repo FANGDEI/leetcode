24a0edfe166a46b89c3b56fad7f9a6db42ec8d56
Devildyw <1842501760@qq.com>
2022-12-02 12:24:51 +0800

    每日一题(有点easy) Date:2022/12/2

---
+++ java-devildyw/src/main/java/com/dyw/leetcode/medium/arrays/L1769移动所有球到每个盒子所需的最小操作数.java
@@ -0,0 +1,33 @@
+package com.dyw.leetcode.medium.arrays;
+
+/**
+ * @author Devil
+ * @since 2022-12-02-11:38
+ */
+@SuppressWarnings("all")
+public class L1769移动所有球到每个盒子所需的最小操作数 {
+    public static void main(String[] args) {
+        int[] rse = new L1769移动所有球到每个盒子所需的最小操作数().minOperations("110");
+        for (int i : rse) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public int[] minOperations(String boxes) {
+        char[] chars = boxes.toCharArray();
+        int[] res = new int[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j]=='1'){
+                    sum += Math.abs(i-j);
+                }
+            }
+            res[i] = sum;
+        }
+
+        return res;
+    }
+
+}
