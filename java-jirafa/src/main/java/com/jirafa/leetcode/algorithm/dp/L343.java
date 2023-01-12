44a8d17f6fb381c243e52d1cfb2c07a44cab74e7
JirafaYe <2582101458@qq.com>
2023-01-12 18:31:49 +0800

    抽象

--- java-jirafa/src/main/java/com/jirafa/leetcode/algorithm/dp/L343.java
+++ java-jirafa/src/main/java/com/jirafa/leetcode/algorithm/dp/L343.java
@@ -1,4 +1,21 @@
 package com.jirafa.leetcode.algorithm.dp;
 
+import org.junit.Test;
+
 public class L343 {
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[2]=1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= i/2; j++) {
+                dp[i]=Math.max(dp[i], Math.max(j*(i-j),j*dp[i-j]));
+            }
+        }
+        return dp[n];
+    }
+
+    @Test
+    public void test(){
+        System.out.println(new L343().integerBreak(10));
+    }
 }
