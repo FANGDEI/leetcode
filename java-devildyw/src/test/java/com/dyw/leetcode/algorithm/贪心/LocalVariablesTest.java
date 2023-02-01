8334b937469517f1b5f6bfafcc7754f2df0a4e9c
Devildyw <1842501760@qq.com>
2023-02-01 19:28:24 +0800

    康复训练

---
+++ java-devildyw/src/test/java/com/dyw/leetcode/algorithm/贪心/LocalVariablesTest.java
@@ -0,0 +1,27 @@
+package com.dyw.leetcode.algorithm.贪心;
+
+import java.util.Date;
+
+/**
+ * @author Devil
+ * @since 2023-01-31-18:06
+ */
+@SuppressWarnings("all")
+public class LocalVariablesTest {
+
+
+
+    public static void main(String[] args) {
+        LocalVariablesTest test = new LocalVariablesTest();
+        int num = 10;
+
+    }
+
+    //练习
+    public static void testStatic(){
+        LocalVariablesTest test = new LocalVariablesTest();
+        Date date = new Date();
+        int count = 10;
+        System.out.println(count);
+    }
+}
