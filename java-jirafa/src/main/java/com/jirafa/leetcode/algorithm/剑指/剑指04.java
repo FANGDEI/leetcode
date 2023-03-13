b5dc163aed7f6342f7c706b407f113b3d7d099a7
JirafaYe <2582101458@qq.com>
2023-03-13 23:33:58 +0800

    今日份的算法

---
+++ java-jirafa/src/main/java/com/jirafa/leetcode/algorithm/剑指/剑指04.java
@@ -0,0 +1,30 @@
+package com.jirafa.leetcode.algorithm.剑指;
+
+public class 剑指04 {
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            int index = search(row, target);
+            if (index >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
--- java-jirafa/src/main/java/com/jirafa/leetcode/algorithm/剑指/剑指53ii.java
+++ java-jirafa/src/main/java/com/jirafa/leetcode/algorithm/剑指/剑指53ii.java
@@ -1,4 +1,27 @@
 package com.jirafa.leetcode.algorithm.剑指;
 
 public class 剑指53ii {
+    public int missingNumber(int[] nums) {
+        int[] newNums = new int[nums.length + 1];
+        for(int i:nums){
+            newNums[i]++;
+        }
+
+        int missing=0;
+
+        for (int i = 0; i < newNums.length; i++) {
+            if(newNums[i]==0) {
+                missing = i;
+                break;
+            }
+        }
+
+        return missing;
+    }
+
+    public static void main(String[] args) {
+        new 剑指53ii().missingNumber(new int[]{1,2,3});
+    }
 }
+
+
