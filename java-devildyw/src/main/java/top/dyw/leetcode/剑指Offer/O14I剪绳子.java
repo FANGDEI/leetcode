6afe999da56013f4d6ebcc18502e791ec917b414
Devildyw <1842501760@qq.com>
2023-05-24 20:05:57 +0800

    还是可以哈

--- java-devildyw/src/main/java/top/dyw/leetcode/剑指Offer/O14I剪绳子.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/剑指Offer/O14I剪绳子.java
@@ -13,7 +13,7 @@ public class O14I剪绳子 {
     /**
      * 动态规划
      */
-    public int cuttingRope(int n) {
+    public int cuttingRope_dp(int n) {
         int[] dp = new int[n + 1]; // dp[i] 表示将正整数i拆分成了至少两个数之后对应的最大乘积
         //dp[0] dp[1] 因为 0 和 1 都是不能再拆分的数所以值为0
 
@@ -26,6 +26,47 @@ public class O14I剪绳子 {
             }
         }
 
+        return dp[n];
+    }
+
+    /**
+     * 贪心算法 性能更好
+     * 尽量将一个数拆成几个三相乘，这样的结果最大
+     */
+    public int cuttingRope_greedy(int n) {
+        //如果小于等于三，拆只能拆成 1*1*1 或者 1*2，所以结果为 n-1
+        if (n <= 3) {
+            return n - 1;
+        }
+
+        //a计算n可以拆成多少的3 b则代表n拆成多个3后剩下的数
+        int a = n / 3, b = n % 3;
+        if (b == 0) { //余0 刚好分够
+            return (int)Math.pow(3, a);
+        } else if (b == 1) { //余1 乘4总比乘3*1 大吧
+            return (int)Math.pow(3, a - 1) * 4;
+        } else { //余2没什么好说的 *2即可
+            return (int)Math.pow(3, a) * 2;
+        }
+    }
+
+
+    /**
+     * dp
+     * @param n
+     * @return
+     */
+    public int cuttingRope_dp01(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        //dp[i] 表示i所拆分成的数乘积的最大值 j*(i-j) 表示将i拆成了i和i-j, j*dp[i-j] (dp[i-j](i-j)表示拆分成数乘积最大值)与j的乘积
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i],Math.max(j*(i-j),j*dp[i-j]));
+            }
+        }
         return dp[n];
     }
 }
