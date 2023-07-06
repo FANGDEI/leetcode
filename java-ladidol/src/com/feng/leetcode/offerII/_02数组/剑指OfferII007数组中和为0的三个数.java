9ec5be47075bd8cb5d65dc36adfcd8694600910a
xiaoqiang-ladidol <599426945@qq.com>
2023-07-06 12:05:08 +0800

    :art: review

--- java-ladidol/src/com/feng/leetcode/offerII/_02数组/剑指OfferII007数组中和为0的三个数.java
+++ java-ladidol/src/com/feng/leetcode/offerII/_02数组/剑指OfferII007数组中和为0的三个数.java
@@ -14,7 +14,7 @@ public class 剑指OfferII007数组中和为0的三个数 {
     //三指针，i,j,k进行范围性地毯式搜索
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            Arrays.sort(nums);
+            Arrays.sort(nums);//从小到大
             List<List<Integer>> res = new LinkedList<>();
             for (int i = 0; i < nums.length; i++) {
                 if (i > 0 && nums[i] == nums[i - 1]) {
@@ -23,6 +23,7 @@ public class 剑指OfferII007数组中和为0的三个数 {
                 int j = i + 1;
                 int k = nums.length - 1;
                 while (j < k) {
+                    //去重
                     if (j > i + 1 && j < nums.length && nums[j] == nums[j - 1]) {
                         j++;//注意不要忘记
                         continue;
--- java-ladidol/src/com/feng/leetcode/offerII/_02数组/剑指OfferII010和为k的子数组.java
+++ java-ladidol/src/com/feng/leetcode/offerII/_02数组/剑指OfferII010和为k的子数组.java
@@ -1,5 +1,8 @@
 package com.feng.leetcode.offerII._02数组;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: Xiaoqiang-Ladidol
  * @date: 2023/5/7 14:48
@@ -8,7 +11,7 @@ package com.feng.leetcode.offerII._02数组;
 public class 剑指OfferII010和为k的子数组 {
 
     //这是朴素做法
-    class Solution {
+    class Solution1 {
         public int subarraySum(int[] nums, int k) {
             int ans = 0;
             for (int i = 0; i < nums.length; i++) {
@@ -27,5 +30,23 @@ public class 剑指OfferII010和为k的子数组 {
     }
 
     //还有一种前缀和+哈希的做法 https://leetcode.cn/problems/QTMn0o/solutions/1764672/by-ac_oier-hr6k/
+    //还有一种前缀和+哈希的做法 https://leetcode.cn/problems/subarray-sum-equals-k/solutions/562174/de-liao-yi-wen-jiang-qian-zhui-he-an-pai-yhyf/
+
+    //前缀和+哈希
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            if (nums.length == 0) return 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);//前缀和为零的情况。
+            int preSum = 0, ans = 0;
+            for (int i = 0; i < nums.length; i++) {
+                preSum += nums[i];
+                ans += map.getOrDefault(preSum - k, 0);
+                map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+            }
+            return ans;
+        }
+    }
+
 
 }
