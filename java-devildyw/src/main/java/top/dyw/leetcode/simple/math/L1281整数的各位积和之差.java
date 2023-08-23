071dbded5ef74b2506f642ffcf0ae1b0d7a6085a
Devildyw <1842501760@qq.com>
2023-08-23 22:27:05 +0800

    :thinking: 补补

--- java-devildyw/src/main/java/top/dyw/leetcode/simple/math/L1281整数的各位积和之差.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/simple/math/L1281整数的各位积和之差.java
@@ -8,7 +8,10 @@ package top.dyw.leetcode.simple.math;
 public class L1281整数的各位积和之差 {
 
     public static void main(String[] args) {
-
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
     }
 
 
