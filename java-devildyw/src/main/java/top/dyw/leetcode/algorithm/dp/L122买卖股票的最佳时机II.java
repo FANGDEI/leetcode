22ada681f0e2e83e5156daaf4beae9bf5d1c0e6d
dingyangwei001 <dingyangwei001@ke.com>
2025-03-10 10:49:56 +0800

    今天贝壳股票涨了 就简单写一个买卖股票的最佳时机II吧

--- java-devildyw/src/main/java/top/dyw/leetcode/algorithm/dp/L122买卖股票的最佳时机II.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/algorithm/dp/L122买卖股票的最佳时机II.java
@@ -8,7 +8,7 @@ package top.dyw.leetcode.algorithm.dp;
 public class L122买卖股票的最佳时机II {
 
     public static void main(String[] args) {
-
+        System.out.println(new L122买卖股票的最佳时机II().maxProfit02(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
     public int maxProfit(int[] prices) {
@@ -24,5 +24,24 @@ public class L122买卖股票的最佳时机II {
         }
 
         return dp[n-1][1];
+    }
+
+    /**
+     * dp[i][0] 是持有股票的利润 i-1 持有股票但是i-1天没有卖出，i-1未持有股票，但是i天购入了
+     * dp[i][1] 是未持有股票的利润 i-1天就未持有股票，第i天也没有买入；i-1天持有股票，但是第i天买出了
+     * @param prices
+     * @return
+     */
+    public int maxProfit02(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], prices[i] + dp[i-1][0]);
+        }
+        return dp[prices.length-1][1];
     }
 }
