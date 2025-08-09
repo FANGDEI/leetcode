d23cf597384ff041563aab61739d48c80afcc5d1
xiaoxiao-ladidol-liang <599426945@qq.com>
2025-08-09 22:28:00 +0800

    回溯

--- java-ladidol/src/com/feng/leetcode/hot100/_06回溯/h77组合.java
+++ java-ladidol/src/com/feng/leetcode/hot100/_06回溯/h77组合.java
@@ -1,6 +1,5 @@
 package com.feng.leetcode.hot100._06回溯;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,20 +11,29 @@ public class h77组合 {
             result = new LinkedList<>();
             LinkedList<Integer> curAns = new LinkedList<>();
             for (int i = 1; i <= n; i++) {
+                // 可以适当剪枝
+                if (n - i + 1 < k - curAns.size()) {
+                    return result;
+                }
                 curAns.addLast(i);
                 dfs(i, curAns, k, n);
                 curAns.removeLast();
             }
             return result;
-            
+
         }
+
         List<List<Integer>> result;
 
-        void dfs(int curIndex, LinkedList<Integer> curAns, int k, int n){
-            if (curAns.size()==k) {
+        void dfs(int curIndex, LinkedList<Integer> curAns, int k, int n) {
+            if (curAns.size() == k) {
                 result.add(new LinkedList<>(curAns));
             }
             for (int i = curIndex + 1; i <= n; i++) {
+                // 可以适当剪枝
+                if (n - i + 1 < k - curAns.size()) {
+                    return;
+                }
                 curAns.addLast(i);
                 dfs(i, curAns, k, n);
                 curAns.removeLast();
