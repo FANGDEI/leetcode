7a689192c6b82742d244298210c7ba304599a951
fengxiaoqiang004 <fengxiaoqiang004@ke.com>
2025-08-19 11:53:00 +0800

    add solutions for h124二叉树中的最大路径和 and h128最长连续序列 with depth-first search and optimized approach

--- java-ladidol/src/com/feng/leetcode/hot100/_04二叉树/h124二叉树中的最大路径和.java
+++ java-ladidol/src/com/feng/leetcode/hot100/_04二叉树/h124二叉树中的最大路径和.java
@@ -19,8 +19,8 @@ public class h124二叉树中的最大路径和 {
       if (root == null) {
         return 0;
       }
-      int leftSum = maxPathSum(root.left);
-      int rightSum = maxPathSum(root.right);
+      int leftSum = dfs(root.left);
+      int rightSum = dfs(root.right);
       ans = Math.max(ans, leftSum + rightSum + root.val);
       // 和0比较，如果比零小，直接断开连接
       return Math.max(Math.max(leftSum, rightSum) + root.val, 0);
