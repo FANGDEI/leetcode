7dd7b12de4f2d98eea816a92235fde66d708f894
dingyangwei001 <dingyangwei001@ke.com>
2026-02-05 18:27:06 +0800

    打卡第81天 放假啦

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L5最长回文子串.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L5最长回文子串.java
@@ -28,4 +28,30 @@ public class L5最长回文子串 {
         }
         return s.substring(begin, begin + maxLen);
     }
+
+    public String longestPalindromeNew(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        dp[0][0] = true;
+        int maxLen = 0;
+        int begin = 0;
+        for (int i=n-1; i>=0; i--) {
+            for (int j=i; j<n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j-i+1 < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+                if (dp[i][j] && maxLen < j-i+1) {
+                    begin = i;
+                    maxLen = j-i+1;
+                }
+            }
+        }
+
+        return s.substring(begin, begin + maxLen);
+
+    }
 }
