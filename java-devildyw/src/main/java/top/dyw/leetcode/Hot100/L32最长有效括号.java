18e26dc6b85dd562e2b88ba2ae726825bbda1c9c
dingyangwei001 <dingyangwei001@ke.com>
2026-02-08 14:16:57 +0800

    打卡第82天 回家了 开始学习！

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L32最长有效括号.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L32最长有效括号.java
@@ -3,24 +3,22 @@ package top.dyw.leetcode.Hot100;
 public class L32最长有效括号 {
     public int longestValidParentheses(String s) {
         int n = s.length();
-        if (n<=1) {
+        if (n < 2) {
             return 0;
         }
-        int[] dp = new int[n];
         int maxLen = 0;
-        // dp[i] 表示以s[i]结尾的最长有效括号数量
-        dp[0] = 0;
+        // 以 s[i] 这个字符结尾的、最长的有效括号子串长度
+        int[] dp = new int[n];
         for (int i=1; i<n; i++) {
-            if (s.charAt(i)==')') {
-                if (s.charAt(i-1)=='(') {
-                    dp[i] = (i-2>=0 ? dp[i-2] : 0) + 2;
-                } else if (i-dp[i-1] > 0 && s.charAt(i - dp[i-1] - 1) == '('){
-                    // 需要把前面的括号数量加在一起
-                    int prevVaild = (i-dp[i-1]>=2) ? dp[i-dp[i-1]-2] : 0;
-                    dp[i] = dp[i-1] + 2 + prevVaild;
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i-1) == '(') {
+                    dp[i] = (i>=2 ? dp[i-2] : 0) + 2;
+                } else if (i-dp[i-1] > 0 && s.charAt(i-dp[i-1]-1) == '(') {
+                    int prevVaild = (i-dp[i-1] >= 2) ? dp[i-dp[i-1]-2] : 0;
+                    dp[i] = prevVaild + 2 + dp[i-1];
                 }
             }
-            maxLen = Math.max(dp[i], maxLen);
+            maxLen = Math.max(maxLen, dp[i]);
         }
         return maxLen;
     }
