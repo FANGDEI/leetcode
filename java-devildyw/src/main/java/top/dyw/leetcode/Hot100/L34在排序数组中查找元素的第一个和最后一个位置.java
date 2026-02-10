454a472720bb9c207e859852f86023119bce859b
dingyangwei001 <dingyangwei001@ke.com>
2026-02-10 22:19:25 +0800

    打卡第84天 在家学习ddd！

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L34在排序数组中查找元素的第一个和最后一个位置.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L34在排序数组中查找元素的第一个和最后一个位置.java
@@ -2,37 +2,41 @@ package top.dyw.leetcode.Hot100;
 
 public class L34在排序数组中查找元素的第一个和最后一个位置 {
     public int[] searchRange(int[] nums, int target) {
-        // 先查找第一个位置
-        int left = 0;
-        int right = nums.length - 1;
+        if (nums.length == 0) {
+            return new int[]{-1,-1};
+        }
+        int[] result = new int[2];
 
-        int first = -1;
+        int left = 0;
+        int right = nums.length-1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
-                left = mid+1;
-            } else {
-                if (nums[mid] == target) {
-                    first = mid;
-                }
-                right = mid - 1;
-            }
-        }
-
-        int second = -1;
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                if (nums[mid] == target) {
-                    second = mid;
-                }
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return new int[]{first, second};
+        if (left >= nums.length || nums[left] != target) {
+            return new int[]{-1,-1};
+        }
+        result[0] = left;
+
+        left = 0;
+        right = nums.length-1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        result[1] = right;
+
+        return result;
     }
 }
