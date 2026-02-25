cb3bf1ba2fceff17dd5e288556bddbf35b18f496
dingyangwei001 <dingyangwei001@ke.com>
2026-02-25 20:17:54 +0800

    打卡第89天 康复训练 简历准备了30%

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L102二叉树的层序遍历.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L102二叉树的层序遍历.java
@@ -1,30 +1,31 @@
 package top.dyw.leetcode.Hot100;
-
+import java.util.*;
 public class L102二叉树的层序遍历 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root==null) {
+        if (root == null) {
             return result;
         }
-        Deque<TreeNode> deque = new LinkedList<>();
-        deque.offer(root);
 
-        while(deque.size()>0) {
-            int size = deque.size();
-            List<Integer> list = new ArrayList<>();
-            while (size > 0) {
-                TreeNode node = deque.poll();
-                list.add(node.val);
-                if (node.left!=null){
-                    deque.offer(node.left);
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            while (size-->0) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left!=null) {
+                    queue.offer(node.left);
                 }
-                if (node.right!=null){
-                    deque.offer(node.right);
+                if (node.right!=null) {
+                    queue.offer(node.right);
                 }
-                size--;
             }
-            result.add(list);
+            result.add(temp);
         }
+
         return result;
     }
 }
