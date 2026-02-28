3da2acf81ea820c73ed6103b9d719a05afbcbf44
dingyangwei001 <dingyangwei001@ke.com>
2026-02-28 18:35:08 +0800

    打卡第90天 写简历写简历

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L105从前序与中序遍历序列构造二叉树.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L105从前序与中序遍历序列构造二叉树.java
@@ -2,29 +2,27 @@ package top.dyw.leetcode.Hot100;
 
 public class L105从前序与中序遍历序列构造二叉树 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0 || inorder.length == 0) {
-            return null;
-        }
-        return buildTreeHelper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+        return buildTreeHelper(preorder, 0, preorder.length-1, inorder, 0, inorder.length - 1);
     }
 
-    public TreeNode buildTreeHelper(int[] preorder, int pLeft, int pRight, int[] inorder, int iLeft, int iRight) {
-        if (pLeft > pRight || iLeft > iRight) {
+    public TreeNode buildTreeHelper(int[] preorder, int pLeft, int pRight, int[] inorder, int iLeft, int iRight){
+        if (pLeft > pRight) {
             return null;
         }
 
-        int rootVal = preorder[pLeft];
-        TreeNode root = new TreeNode(rootVal);
-        int index = -1;
-        for (int i = iLeft; i<=iRight; i++) {
-            if (inorder[i] == rootVal) {
-                index = i;
+        TreeNode root = new TreeNode(preorder[pLeft]);
+
+        int index = iLeft;
+        while (index<=iRight) {
+            if (inorder[index] == root.val) {
                 break;
             }
+            index++;
         }
+        int leftSize = index - iLeft;
+        root.left = buildTreeHelper(preorder, pLeft+1, pLeft+leftSize, inorder, iLeft, index-1);
+        root.right = buildTreeHelper(preorder, pLeft+leftSize+1, pRight, inorder, index+1, iRight);
 
-        root.left = buildTreeHelper(preorder, pLeft + 1, pLeft + index - iLeft, inorder, iLeft, index-1);
-        root.right = buildTreeHelper(preorder, pLeft + index - iLeft + 1, pRight, inorder, index+1, iRight);
         return root;
     }
 }
