331daee45594b2bb82334c5e1fdcc2dc61ae1992
dingyangwei001 <dingyangwei001@ke.com>
2026-03-11 21:12:53 +0800

    打卡第93天 开始投简历 先投小厂

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L2两数相加.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L2两数相加.java
@@ -2,30 +2,20 @@ package top.dyw.leetcode.Hot100;
 
 public class L2两数相加 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        }
-
-        ListNode dummyNode = new ListNode(-1, null);
-        ListNode cur = dummyNode;
+        ListNode dummyNode = new ListNode(-1);
+        ListNode p = dummyNode;
         int c = 0;
-        while (l1 != null || l2 != null || c!=0) {
-            int val1 = l1 == null ? 0 : l1.val;
-            int val2 = l2 == null ? 0 : l2.val;
+        while (l1!=null || l2!=null || c!=0) {
+            int val1 = l1==null?0:l1.val;
+            int val2 = l2==null?0:l2.val;
+
+            l1 = l1==null?null:l1.next;
+            l2 = l2==null?null:l2.next;
 
             int sum = val1 + val2 + c;
-            cur.next = new ListNode(sum%10, null);
-            cur = cur.next;
+            p.next = new ListNode(sum%10);
+            p = p.next;
             c = sum / 10;
-
-
-            if (l1!=null) {
-                l1 = l1.next;
-            }
-
-            if (l2!=null) {
-                l2 = l2.next;
-            }
         }
 
         return dummyNode.next;
