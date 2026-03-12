4c4aa8fc2a3e3996bf67036fa6b0945284d24938
dingyangwei001 <dingyangwei001@ke.com>
2026-03-12 21:57:32 +0800

    打卡第94天 陆续有面试了 👀八股

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L138随机链表的复制.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L138随机链表的复制.java
@@ -1,29 +1,44 @@
 package top.dyw.leetcode.Hot100;
 
 public class L138随机链表的复制 {
-    public Node copyRandomList(Node head) {
-        Node dummyNode = new Node(-1);
-        Node cur = dummyNode;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummyNode = new ListNode(-1, head);
+        ListNode pre = dummyNode;
+        ListNode cur = dummyNode;
 
-        HashMap<Node, Node> map = new HashMap<>();
-
-        while(head != null) {
-            Node newNode = new Node(head.val);
-            newNode.random = head.random;
-            map.put(head, newNode);
-            head = head.next;
-            cur.next = newNode;
-            cur = cur.next;
-        }
-
-        cur = dummyNode.next;
-        while(cur!=null){
-            if (cur.random != null) {
-                cur.random = map.get(cur.random);
+        while (cur != null) {
+            int n = k;
+            while (cur != null && n-->0) {
+                cur = cur.next;
             }
-            cur = cur.next;
+
+
+            if (cur == null) {
+                break;
+            }
+
+            ListNode next = cur.next;
+            cur.next = null;
+            ListNode tail = pre.next;
+
+            pre.next = reverseList(tail);
+            tail.next = next;
+            cur = tail;
+            pre = tail;
         }
 
         return dummyNode.next;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head!=null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+
+        return newHead;
     }
 }
