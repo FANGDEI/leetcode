b69893f233d15057dcd7d19a6d05db33c679d943
dingyangwei001 <dingyangwei001@ke.com>
2026-03-15 00:26:50 +0800

    打卡第95天 好多东西没准备好啊 感觉第一周的面试极大概率很失败 但是不能放弃啊

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L148排序链表.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L148排序链表.java
@@ -2,72 +2,76 @@ package top.dyw.leetcode.Hot100;
 
 public class L148排序链表 {
     public ListNode sortList(ListNode head) {
-        // 合并两个有序链表
-        ListNode dummyNode = new ListNode(-1, head);
-        ListNode cur = head;
-        int length = 0;
-        while(cur!=null) {
-            length++;
-            cur = cur.next;
+        if (head == null) {
+            return null;
         }
 
+        int length = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            length++;
+        }
 
-        int sublength = 1;
-        while (sublength < length) {
+        ListNode dummyNode = new ListNode(-1,head);
+
+        int subLength = 1;
+        while (subLength < length) {
             ListNode pre = dummyNode;
             cur = dummyNode.next;
-            while (cur!=null) {
+            while (cur != null) {
                 ListNode head1 = cur;
-                for (int i=1; i<sublength&&cur.next!=null; i++) {
+                for (int i=1; i<subLength&&cur.next!=null; i++) {
                     cur = cur.next;
                 }
                 ListNode head2 = cur.next;
                 cur.next = null;
                 cur = head2;
-                for (int i = 1; i<sublength&&cur!=null; i++) {
+                for (int i=1; i<subLength&&cur!=null; i++) {
                     cur = cur.next;
                 }
 
                 ListNode next = null;
-                if (cur!=null) {
+                if (cur != null) {
                     next = cur.next;
                     cur.next = null;
                 }
 
-                // 至此断开了两个链表了 开始merge
                 pre.next = mergeList(head1, head2);
-                // 进入下一个循环
-                cur = next;
 
-                while (pre.next!=null) {
+                cur = next;
+                while (pre.next != null) {
                     pre = pre.next;
                 }
             }
-            sublength <<= 1;
+            subLength<<=1;
         }
         return dummyNode.next;
+
     }
 
-    public ListNode mergeList(ListNode l1, ListNode l2) {
-        ListNode dummyNode = new ListNode(-1, null);
-        ListNode cur = dummyNode;
+    public ListNode mergeList(ListNode head1, ListNode head2) {
+        ListNode dummyNode = new ListNode(-1);
 
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
+        ListNode cur = dummyNode;
+        while (head1!=null && head2!=null) {
+            if (head1.val < head2.val) {
+                cur.next = new ListNode(head1.val);
+                head1 = head1.next;
             } else {
-                cur.next = l2;
-                l2 = l2.next;
+                cur.next = new ListNode(head2.val);
+                head2 = head2.next;
             }
+
             cur = cur.next;
         }
 
-        if (l1 != null) {
-            cur.next = l1;
+        if (head1!=null) {
+            cur.next = head1;
         }
-        if (l2 != null) {
-            cur.next = l2;
+
+        if (head2!= null) {
+            cur.next = head2;
         }
 
         return dummyNode.next;
