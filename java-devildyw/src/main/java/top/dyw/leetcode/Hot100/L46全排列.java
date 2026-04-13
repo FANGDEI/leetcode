ffa5891df5d967c38195b28d55c6d6d056edaaf9
Devildyw <devildyw.top@gmail.com>
2026-04-13 22:42:56 +0800

    打卡第109天 周末生病了 今天周一也还有点难受 明天面试 希望明天还有时间查漏补缺。

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L46全排列.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L46全排列.java
@@ -3,26 +3,28 @@ package top.dyw.leetcode.Hot100;
 import java.util.*;
 
 public class L46全排列 {
-    List<List<Integer>> rseult = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
+    HashSet<Integer> set = new HashSet<>();
     List<Integer> path = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        dfs(nums, set);
-        return rseult;
+        dfs(nums);
+        return result;
     }
 
-    public void dfs(int[] nums, Set<Integer> set) {
+    public void dfs(int[] nums) {
         if (path.size() == nums.length) {
-            rseult.add(new ArrayList(path));
+            result.add(new ArrayList(path));
             return;
         }
 
-        for (int i = 0; i<nums.length; i++) {
-            if (!set.add(nums[i])) {
+        for (int i=0; i<nums.length; i++) {
+            if (set.contains(nums[i])) {
                 continue;
             }
+
+            set.add(nums[i]);
             path.add(nums[i]);
-            dfs(nums, set);
+            dfs(nums);
             path.remove(path.size()-1);
             set.remove(nums[i]);
         }
