fcc71e95fe4c60788acba06a10244932fd42f205
Devildyw <devildyw.top@gmail.com>
2026-04-22 22:22:13 +0800

    打卡第112天 身体还是很虚弱 明天加油。

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L739每日温度.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L739每日温度.java
@@ -3,14 +3,14 @@ package top.dyw.leetcode.Hot100;
 import java.util.*;
 public class L739每日温度 {
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> stack = new ArrayDeque();
         int n = temperatures.length;
         int[] result = new int[n];
 
+        Deque<Integer> stack = new ArrayDeque<>();
         for (int i=0; i<n; i++) {
-            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
                 int index = stack.pop();
-                result[index] = i-index;
+                result[index] = i - index;
             }
             stack.push(i);
         }
