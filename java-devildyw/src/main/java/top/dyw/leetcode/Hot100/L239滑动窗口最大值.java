92dd69bb41ef07ea102abb5f4fe779f32411aad4
dingyangwei001 <dingyangwei001@ke.com>
2026-05-10 22:27:56 +0800

    打卡第114天 明天加油 今天恶补一下 虽然简历不出众但是靠其他地方给面试官眼前一亮吧

--- java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L239滑动窗口最大值.java
+++ java-devildyw/src/main/java/top/dyw/leetcode/Hot100/L239滑动窗口最大值.java
@@ -4,26 +4,22 @@ import java.util.PriorityQueue;
 // 堆来做 这种滑动取最大值的场景 惰性删除
 public class L239滑动窗口最大值 {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n-k+1];
 
-
-        int start = 0;
-        int[] result = new int[nums.length-k+1];
-        int index = 0;
-        // 大顶堆 堆顶是整个堆的最大值
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b)->{
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> {
             return b[0] - a[0];
         });
 
-        for(int i=0; i<k; i++) {
-            maxHeap.offer(new int[]{nums[i],i});
+        for (int i=0; i<k; i++) {
+            maxHeap.offer(new int[]{nums[i], i});
         }
-
+        int index = 0;
         result[index++] = maxHeap.peek()[0];
 
-        for(int i=k; i<nums.length; i++) {
-            maxHeap.offer(new int[]{nums[i],i});
-            start++;
-            while(start > maxHeap.peek()[1]) {
+        for (int i=k; i<n; i++) {
+            maxHeap.offer(new int[]{nums[i], i});
+            while (maxHeap.peek()[1] < index) {
                 maxHeap.poll();
             }
             result[index++] = maxHeap.peek()[0];
