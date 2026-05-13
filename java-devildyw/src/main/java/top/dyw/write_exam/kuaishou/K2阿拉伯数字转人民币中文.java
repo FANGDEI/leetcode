aa9a453eb77d76c353285770af202ef9fa71d25e
dingyangwei001 <dingyangwei001@ke.com>
2026-05-13 21:13:00 +0800

    打卡第116天 今天把需求都写完了，明天可以好好准备一天 感觉算法很重要 二面感觉面试官又会问一些质量保障、稳定性之类的东西 还有就是自己的职业规划 算法上次没写出来 加油啊

--- java-devildyw/src/main/java/top/dyw/write_exam/kuaishou/K2阿拉伯数字转人民币中文.java
+++ java-devildyw/src/main/java/top/dyw/write_exam/kuaishou/K2阿拉伯数字转人民币中文.java
@@ -3,7 +3,7 @@ package top.dyw.write_exam.kuaishou;
 public class K2阿拉伯数字转人民币中文 {
 
     public static void main(String[] args) {
-        System.out.println(numberToChinese(101));
+        System.out.println(numberToChinese(90000009));
     }
 
     // 中文数字
@@ -23,17 +23,16 @@ public class K2阿拉伯数字转人民币中文 {
 
     public static String numberToChinese(int num) {
         if (num == 0) {
-            return "零元整";
+            return "零元";
         }
 
         StringBuilder sb = new StringBuilder();
-        int groupIndex = 0;
         boolean needZero = false;
+        int groupIndex = 0;
 
-        while(num > 0) {
-            //每次取4位
+        while (num > 0) {
             int part = num % 10000;
-            if (part != 0) {
+            if (part!=0) {
                 String partStr = convertPart(part);
 
                 if (needZero) {
@@ -42,30 +41,24 @@ public class K2阿拉伯数字转人民币中文 {
 
                 sb.insert(0, partStr + GROUP_UNITS[groupIndex]);
 
-                // 当前组不足4位
                 needZero = part < 1000;
             } else {
                 needZero = false;
             }
-
             num /= 10000;
             groupIndex++;
         }
+
         sb.append("元");
         return sb.toString();
     }
 
-    /**
-     * 转换0～9999
-     */
-    private static String convertPart(int num) {
-        StringBuilder sb = new StringBuilder();
-
+    public static String convertPart(int part) {
         int unitIndex = 0;
         boolean zeroFlag = false;
-
-        while (num > 0) {
-            int digit = num % 10;
+        StringBuilder sb = new StringBuilder();
+        while (part > 0) {
+            int digit = part % 10;
             if (digit == 0) {
                 if (!zeroFlag && sb.length() > 0) {
                     sb.insert(0, "零");
@@ -76,10 +69,11 @@ public class K2阿拉伯数字转人民币中文 {
                 sb.insert(0, NUMS[digit] + UNITS[unitIndex]);
                 zeroFlag = false;
             }
-
             unitIndex++;
-            num/=10;
+            part/=10;
         }
         return sb.toString();
     }
+
+
 }
