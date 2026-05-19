f02268cd983dc7e4333faad8e40c86d860060705
dingyangwei001 <dingyangwei001@ke.com>
2026-05-19 19:43:08 +0800

    打卡第120天 感觉不太稳啊 希望能保温吧。

--- java-devildyw/src/main/java/top/dyw/write_exam/kuaishou/K3最长递增子序列.java
+++ java-devildyw/src/main/java/top/dyw/write_exam/kuaishou/K3最长递增子序列.java
@@ -5,21 +5,24 @@ import java.util.Arrays;
 
 public class K3最长递增子序列 {
 
+    public static void main(String[] args) {
+        System.out.println(new K3最长递增子序列().lengthOfLIS(new int[]{1, 2, 3, 10, 5, 6, 7, 8}));
+    }
+
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
+        int[] dp = new int[nums.length];
 
-        int[] dp = new int[n];
-
-        Arrays.fill(dp, 1);
-
+        for (int i=0; i<nums.length; i++) {
+            dp[i] = 1;
+        }
         int ans = 0;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<nums.length; i++) {
             for (int j=0; j<i; j++) {
                 if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
-            ans = Math.max(ans, dp[i]);
+            ans = Math.max(dp[i], ans);
         }
         return ans;
     }
