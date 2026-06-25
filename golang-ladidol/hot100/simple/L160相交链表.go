1ed35f8069e854cd3ccb44706c2d722ea06968b9
fengxiaoqiang <fengxiaoqiang@didiglobal.com>
2026-06-25 20:25:24 +0800

    打卡第三天：康复训练

--- golang-ladidol/hot100/simple/L160相交链表.go
+++ golang-ladidol/hot100/simple/L160相交链表.go
@@ -1,9 +1,6 @@
 package simple
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	if headA == nil || headB == nil {
-		return nil
-	}
 	nodeA, nodeB := headA, headB
 
 	// 遍历两遍以内，直到这两个节点相等
